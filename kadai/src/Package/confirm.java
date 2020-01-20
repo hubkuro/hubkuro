fd1967f7621008403879384b174c7e9e870b45c8
Taiyo Hirata <183204@ocsjoho.onmicrosoft.com>
2020-01-20 12:07:57 +0900

    first comit

---
+++ kadai/src/Package/confirm.java
@@ -0,0 +1,21 @@
+package Package;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Confirm
+ */
+@WebServlet("/Confirm")
+public class confirm extends HttpServlet {
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
