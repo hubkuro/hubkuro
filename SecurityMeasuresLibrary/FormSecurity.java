1368506bbab66378c13834252cfb7b49a3162235
himihiromu <himihiromu@icloud.com>
2020-02-05 16:32:05 +0900

    hozon

---
+++ SecurityMeasuresLibrary/FormSecurity.java
@@ -0,0 +1,37 @@
+import java.util.Random;
+
+public class FormSecurity {
+
+	public String oneTimeToken(HttpSession session) {
+		Random r = new Random();
+		int c;
+		StringBuilder buf = new StringBuilder();
+		char moji;
+		for(int i = 0 ; i < 30 ; i++){
+			
+			c = r.nextInt(3);
+			switch (c) {
+        			case 0:
+            				moji = "0";
+					moji += r.nextInt(9);
+            				break;
+				case 1:
+					moji = "a";
+					moji += r.nextInt(25);
+					break;
+				case 2;
+					moji = "A";
+					moji += r.nextInt(25);
+					break;
+        		}
+			buf.append(moji);
+		}
+		session.setAttribute("token", buf.toString());
+		return buf.toString();
+	}
+
+	public boolean tokenCheck(String token, HttpSession session) {
+		return token.equals(session.setAttribute("token"));
+	}
+
+}
---
+++ upload/RandomPassSet.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+
+public class RandomPassSet {
+
+	public String Set() {
+		Random r = new Random();
+		int c;
+		StringBuilder buf = new StringBuilder();
+		char moji;
+		for(int i = 0 ; i < 10 ; i++){
+			
+			c = r.nextInt(4);
+			switch (c) {
+        			case 0:
+            				moji = "0";
+					moji += r.nextInt(9);
+            				break;
+				case 1:
+					moji = "a";
+					moji += r.nextInt(25);
+					break;
+				case 2;
+					moji = "A";
+					moji += r.nextInt(25);
+					break;
+				case 3;
+					moji = "_";
+					break;
+        		}
+			buf.append(moji);
+		}
+		return buf.toString();
+	}
+
+}
